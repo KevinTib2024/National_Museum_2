4fe81cccdb54dfe0e2d9c12c2b60bb0c6f440cd4
KevinTib2024 <ktibaquicha@ucundinamarca.edu.co>
2024-09-14 01:18:41 -0500

    CreateEntity

---
+++ National_Museum_2/Context/MuseumDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using National_Museum_2.Model;
+
+namespace National_Museum_2.Context
+{
+    public class MuseumDbContext : DbContext
+    {
+        public MuseumDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
+        }
+        public DbSet<User> users { get; set; }
+
+    }
+}
---
+++ National_Museum_2/Model/User.cs
@@ -0,0 +1,13 @@
+﻿namespace National_Museum_2.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
