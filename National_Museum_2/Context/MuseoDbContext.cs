f926eb46d36981583b08b505fbced7b9704f4f0f
KevinTib2024 <ktibaquicha@ucundinamarca.edu.co>
2024-09-14 00:10:26 -0500

    createEntity
    The user entity is added

---
+++ National_Museum_2/Context/MuseoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using National_Museum_2.Model;
+
+namespace National_Museum_2.Context
+{
+    public class MuseoDbContext : DbContext
+    {
+        public MuseoDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<User> users { get; set; }
+
+    }
+}
