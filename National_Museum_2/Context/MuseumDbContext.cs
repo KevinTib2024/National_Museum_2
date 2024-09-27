f1bb7452b369874b120924f1ca6f3682c1ff86cf
malutorres27 <marialtorres@ucundinamarca.edu.co>
2024-09-27 17:44:47 -0500

    Entity Changes_L

--- National_Museum_2/Context/MuseumDbContext.cs
+++ National_Museum_2/Context/MuseumDbContext.cs
@@ -22,35 +22,20 @@ namespace National_Museum_2.Context
 
              modelBuilder.Entity<Permissions>()
                  .HasKey(u => u.permissionsId);
-            /*
-             modelBuilder.Entity<UserType>()
-               .HasKey(u => u.userType);
-
-             modelBuilder.Entity<Gender>()
-               .HasKey(u => u.genderId);
-
-            */  
+   
             modelBuilder.Entity<Contact>()
                    .HasKey(c => c.contactId);
 
-            // modelBuilder.Entity<IdentificationType>()
-              //    .HasKey(c => c.identificationTypeId);
-            /*
 
-             // Relación uno a muchos entre User y Contact
-             modelBuilder.Entity<Contact>()
-                 .HasOne(c => c.User)
-                 .WithMany(u => u.Contacts)
-                 .HasForeignKey(c => c.userId);  // Clave foránea en Contac*/
         }
 
         public DbSet<User> user { get; set; }
-        /*public DbSet<UserType> userType { get; set; }
-        public DbSet<UserType> permissionXUserType { get; set; }
-        public DbSet<UserType> permissions { get; set; }
-        public DbSet<UserType> identificationType { get; set; }
-        public DbSet<UserType> gender { get; set; }
-        public DbSet<UserType> contact { get; set; }*/
+        public DbSet<UserType> userType { get; set; }
+        public DbSet<PermissionXUserType> permissionXUserType { get; set; }
+        public DbSet<Permissions> permissions { get; set; }
+        public DbSet<IdentificationType> identificationType { get; set; }
+        public DbSet<Gender> gender { get; set; }
+        public DbSet<Contact> contact { get; set; }
 
 
     }
