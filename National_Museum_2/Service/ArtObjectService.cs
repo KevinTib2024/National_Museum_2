f60fd4efbe57a001963b3b5e3cd9b02054d181e3
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2024-10-01 21:07:22 -0500

    Create 2 Services

--- National_Museum_2/Service/ArtObjectService.cs
+++ National_Museum_2/Service/ArtObjectService.cs
@@ -1,18 +1,41 @@
 ﻿using National_Museum_2.Model;
+using National_Museum_2.Respositoy;
 
 namespace National_Museum_2.Service
 {
-<<<<<<< HEAD:National_Museum_2/Service/EmployeeService.cs
-    public interface EmployeeService
-=======
-    public class ArtObjectService
->>>>>>> KevinRamirez:National_Museum_2/Service/ArtObjectService.cs
+    public interface IArtObjectService
     {
-        Task<IEnumerable<Employees>> GetAllEmployeesAsync();
-        Task<Employees> GetEmployeesAsync(int id);
-        Task CreateEmployeesAsync(Employees employees);
-        Task UpdateEmployeesAsync(Employees employees);
-        Task SoftDeleteEmployeesAsync(int id);
+        Task<IEnumerable<ArtObject>> GetAllArtObjectAsync();
+        Task<ArtObject> GetArtObjectByIdAsync(int id);
+        Task CreateArtObjectAsync(ArtObject artObject);
+        Task UpdateContactAsync(ArtObject artObject);
+        Task SoftDeleteArtObjectAsync(int id);
     }
+    public class ArtObjectService : IArtObjectService
+    {
+        public Task CreateArtObjectAsync(ArtObject artObject)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<IEnumerable<ArtObject>> GetAllArtObjectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ArtObject> GetArtObjectByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SoftDeleteArtObjectAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateContactAsync(ArtObject artObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
