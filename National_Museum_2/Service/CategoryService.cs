9799ea66cf474e503259076dfa090b8a02e290e5
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2024-10-01 21:26:19 -0500

    Crerate New Service, category

---
+++ National_Museum_2/Service/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using National_Museum_2.Model;
+using National_Museum_2.Respository;
+
+namespace National_Museum_2.Service
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAllCategoryAsync();
+        Task<Category> GetCategoryByIdAsync(int id);
+        Task CreateCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task SoftDeleteCategoryAsync(int id);
+    }
+    public class CategoryService : ICategoryService
+    {
+        public Task CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllCategoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategoryByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SoftDeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
