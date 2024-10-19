0da84bbb4fc6a9105319d9c2eb9c0440398a6c43
Kevin200519 <Ksramirez@ucundinamarca.edu.co>
2024-10-19 12:21:41 -0500

    Revert "Fix CORS problem"
    This reverts commit 623622240d80488fee70ce147891388e13f518d0.

--- National_Museum_2/Program.cs
+++ National_Museum_2/Program.cs
@@ -5,15 +5,6 @@ using National_Museum_2.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5173");
-        });
-});
-
 // Add services to the container.
 var conString = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<MuseumDbContext>(options => options.UseSqlServer(conString));
@@ -96,7 +87,6 @@ app.Environment.IsDevelopment();
 }
 
 app.UseHttpsRedirection();
-app.UseCors();
 
 app.UseAuthorization();
 
